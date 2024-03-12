20cf0cc51ad76b51754ed708dd103e41be14aff2
Chamod De SIlva <chamodlakshitha12@gmail.com>
2024-03-12 12:41:46 +0530

    modified

--- testFunc.py
+++ testFunc.py
@@ -4,7 +4,7 @@ import test
 class TestTest(unittest.TestCase):
     #test add function
     def test_add(self):
-        self.assertEqual(test.add(), 30)
+        self.assertEqual(test.add(), 40)
 
 if __name__ == '__main__':
     unittest.main()
